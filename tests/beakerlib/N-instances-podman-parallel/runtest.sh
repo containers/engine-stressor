b8a03aa8c1c61e8771f1137476e3978e8140e25c
Douglas Landgraf <dlandgra@redhat.com>
2024-06-23 23:15:10 -0400

    test: add beakerlib test for N-podman-instances
    Signed-off-by: Douglas Landgraf <dlandgra@redhat.com>

---
+++ tests/beakerlib/N-instances-podman-parallel/runtest.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# vim: set ft=sh:
+# Description: Clones the engine-stressor repository, installs and run it.
+
+# Include BeakerLib environment
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+PACKAGE="engine-stressor"
+REPO="https://github.com/containers/engine-stressor"
+
+rlJournalStart
+    rlPhaseStartSetup
+        rlLogInfo "Ensuring Podman is installed"
+        if ! command -v podman &> /dev/null; then
+            rlLogInfo "Podman is not installed. Installing Podman."
+            rlRun "sudo dnf install -y podman" 0 "Install Podman"
+        fi
+
+        rlLogInfo "Cloning the repository"
+        rlRun "git clone $REPO" 0 "Clone the engine-stressor repository"
+
+        rlLogInfo "Navigating to the cloned repository directory"
+        rlRun "cd engine-stressor" 0 "Change to the repository directory"
+
+        rlLogInfo "Installing dependencies"
+        rlRun "sudo make install" 0 "Execute sudo make install"
+    rlPhaseEnd
+
+    rlPhaseStartTest
+        rlLogInfo "Running engine-stressor with specified parameters"
+        rlRun 'sudo -E bash -c "TOTAL_MEMORY_FOR_THE_NAMESPACE=\"1G\" \
+            LIST_CURRENT_STATE=true \
+            VERBOSE=true \
+            CLEANUP=false \
+            TIMEOUT_PODMAN_STOP_CONTAINER=5 \
+            NETWORK_NAME=\"my-network\" \
+            VOLUME_NAME=\"my-volume\" \
+            IMAGE_NAME=\"quay.io/centos-sig-automotive/automotive-osbuild\" \
+            IMAGE_COMMAND=\"sleep 3600\" \
+            NUMBER_OF_CONTAINERS=\"100\" \
+            ./engine-stressor"' 0 "Run engine-stressor with the specified environment variables"
+    rlPhaseEnd
+
+    rlPhaseStartCleanup
+        rlLogInfo "Cleaning up"
+        rlRun "cd .."
+        rlRun "rm -rf engine-stressor" 0 "Remove cloned repository"
+        rlRun "podman network rm my-network --force"
+        rlRun "podman volume rm my-volume --force"
+    rlPhaseEnd
+rlJournalEnd
